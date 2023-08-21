4a99b9f802d3902c411e7c6fa8316dc69e51f809
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-21 14:46:37 +0200

    Fix critical alert from SC

--- fj-daogen-base/src/main/java/org/fugerit/java/daogen/base/config/DaogenHelperGenerator.java
+++ fj-daogen-base/src/main/java/org/fugerit/java/daogen/base/config/DaogenHelperGenerator.java
@@ -2,6 +2,8 @@ package org.fugerit.java.daogen.base.config;
 
 public class DaogenHelperGenerator {
 
+	private DaogenHelperGenerator() {}
+	
 	public static String toHelperSourceFolder( DaogenCatalogConfig config ) {
 		return config.getGeneralProps().getProperty( DaogenCatalogConstants.GEN_PROP_SRC_HELPERS, DaogenCatalogConstants.GEN_PROP_SRC_HELPERS_MAIN );
 	}
