90cd3580def5f1f54ca14474610225e1b3a48856
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-02-23 23:34:43 +0100

    fix to jee target

--- fj-daogen-base/src/main/java/org/fugerit/java/daogen/base/gen/helper/RestLoadRealGenerator.java
+++ fj-daogen-base/src/main/java/org/fugerit/java/daogen/base/gen/helper/RestLoadRealGenerator.java
@@ -1,17 +1,24 @@
 package org.fugerit.java.daogen.base.gen.helper;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import org.fugerit.java.daogen.base.config.DaogenCatalogConstants;
 
 public class RestLoadRealGenerator extends BaseRestLoadRealGenerator {
 
 	public static final String KEY = RestLoadRealGenerator.class.getSimpleName();
-	
-	private static final String[] IMPORTS = { "javax.ws.rs.Path" };
+	@Override
+	public Collection<String> getImportList() {
+		String jeeTarget = this.getJeeTargetMode();
+		super.getImportList().add( jeeTarget+".ws.rs.Path" );
+		return super.getImportList();
+	}
 
 	public RestLoadRealGenerator() {
-		super( KEY, Arrays.asList( IMPORTS ), DaogenCatalogConstants.GEN_PROP_PACKAGE_REST_LOAD, null, "@Path(\"/{0}/load\")" );
+		super( KEY, new ArrayList<>(), DaogenCatalogConstants.GEN_PROP_PACKAGE_REST_LOAD, null, "@Path(\"/{0}/load\")" );
 	}
 
 }
